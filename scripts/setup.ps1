fe34cbece9d5213f78fe5619ea095285d6cb2d52
Nikki Yodo <nik_yodo@yahoo.com>
2024-04-14 15:38:57 -0500

    initial commit

---
+++ scripts/setup.ps1
@@ -0,0 +1,7 @@
+# Recommended
+winget install Microsoft.VisualStudioCode
+winget install Microsoft.VisualStudio.2022.Community
+winget install Docker.DockerDesktop
+
+# Optional
+winget install Microsoft.WindowsTerminal
